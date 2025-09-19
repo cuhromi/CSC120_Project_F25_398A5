7b59ac9113d8e6f5272e1490ac497a257e1d4ed8
cuhromi <dmoore83@broncos.uncfsu.edu>
2025-09-19 11:16:04 -0400

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+game_name = "Escaping"
+print("Welcome to", game_name, "!")
+print("====================")
+print("Before we begin, what is your character's name?")
+
+name = input()
+print("Great,", name, "!", "Let's begin the adventure!")
