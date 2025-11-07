88ba06b4dc624459b520e141047b1bd5b01878ef
cuhromi <dmoore83@broncos.uncfsu.edu>
2025-11-07 12:34:10 -0500

    Add player movement and event handling
    Refactored game logic to include player movement and UI.

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
 import random
-game_name = "Escaping"
-print(f"Welcome to {game_name}!")
-print("====================")
-print("Before we begin, what is your character's name?")
 
-name = input()
+# Game Configuration
+game_name = "Escaping"
+name = "Tester"  
+map_size = 9     
+
 
 player = {
-  'name': name,
-  'health': 100,
-  'coin': 0
+    "name": name,
+    "health": 100,
+    "coin": 0,
+    "x": 0,      
+    "y": 0       
 }
 
-print(f"Great {player['name']}! Let's begin the adventure!")
 events = ["find a coin", "meet a monster", "do nothing"]
 
-# complete this line to randomly choose one event from the list 'events'
-event = random.choice(events)
-print(f"While exploring, you {event}!")
-if event == "find a coin":
-  player['coin'] += 1
-  print(f"{player['name']} found a coin, {player['name']} now has {player['coin']} coins.")
-elif event == "meet a monster":
-  player['health'] -= 10
-  print(f"{player['name']} got hurt during the combat with monster, health is now {player['health']}.")
-  
+
+def check_event():
+    global player, events
+    event = random.choice(events)
+    
+    if event == "find a coin":
+        player["coin"] += 1
+    elif event == "meet a monster":
+        player["health"] -= 10
+
+
+def draw_ui(x, y):
+    print("=========================")
+    for i in range(map_size):
+        for j in range(map_size):
+            if i == x and j == y:
+                print("C", end=" ")
+            elif i == map_size - 1 and j == map_size - 1:
+                print("M", end=" ")
+            else:
+                print(".", end=" ")
+        print()  
+    print("=========================")
+    print(f"Health: {player['health']}")
+    print("-------------------------")
+    print(f"Coin: {player['coin']}")
+    print("=========================")
+    print("Your move (w/a/s/d/q):", end="")
+
+
+def move(direction):
+    if direction == 'w' and player['x'] > 0:
+        player['x'] -= 1
+    elif direction == 's' and player['x'] < map_size - 1:
+        player['x'] += 1
+    elif direction == 'a' and player['y'] > 0:
+        player['y'] -= 1
+    elif direction == 'd' and player['y'] < map_size - 1:
+        player['y'] += 1
+    else:
+        print("You cannot move that way!")
+
+
+def main():
+    draw_ui(player['x'], player['y'])
+    direction = input("Your next move (w/a/s/d/q)")
+    
+    while direction != 'q':
+        move(direction)
+        
+        if player['x'] == map_size - 1 and player['y'] == map_size - 1:
+            print("Congratulations! You reach the gate for next level.")
+            break
+            
+        check_event()
+        
+        draw_ui(player['x'], player['y'])
+        direction = input("Your next move (w/a/s/d/q)")
+
+
+if __name__ == '__main__':
+    main()
+
+
 
